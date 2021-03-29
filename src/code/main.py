c5b85a823b828ce16bd832249c3365e326977445
zhen wu <wuzhenw@sfu.ca>
2021-03-29 15:39:59 +0800

    print @ symbol but cannot close the window somehow

--- src/code/main.py
+++ src/code/main.py
@@ -2,8 +2,26 @@
 import tcod
 
 
-def main():
-    print("Hello World!")
+def main() -> None:
+    screen_width = 80
+    screen_height = 50
+
+    player_x = int(screen_width/2)
+    player_y = int(screen_height/2)
+
+    tileset = tcod.tileset.load_tilesheet(
+    	"../picture/font.png", 32, 8, tcod.tileset.CHARMAP_TCOD)
+
+    with tcod.context.new_terminal(screen_width,screen_height,tileset=tileset,title="Roguelike Game",vsync=True)as context:
+    	root_console = tcod.Console(screen_width, screen_height, order="F")
+    	
+    	while True:
+	    	root_console.print(x=player_x, y=player_y, string="@")
+	    	context.present(root_console)
+
+	    	for event in tcod.event.wait():
+	    		if event == "Quit":
+	    			raise SystemExit()
 
 
 if __name__ == "__main__":
