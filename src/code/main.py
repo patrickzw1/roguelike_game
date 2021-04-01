91f3839f444127713f19b7f786b24bcf7a74eb57
zhen wu <patrickzhenwu@gmail.com>
2021-04-01 19:37:21 +0800

    set up game map weight and height, add it to engine function

--- src/code/main.py
+++ src/code/main.py
@@ -4,10 +4,14 @@ from engine import Engine
 from entity import Entity
 from actions import EscapedAction, MovementAction
 from input_handlers import EventHandler
+from game_map import GameMap
 
 def main() -> None:
     screen_width = 80
     screen_height = 50
+
+    map_width = 80
+    map_height = 45
 
     # loading the font image
     tileset = tcod.tileset.load_tilesheet(
@@ -18,7 +22,8 @@ def main() -> None:
     player = Entity(int(screen_width/2), int(screen_height/2), "@", (255,255,255))
     npc = Entity(int(screen_width/2 - 5), int(screen_height/2), "#", (255,255,0))
     entities = {npc, player}
-    engine = Engine(entities=entities, event_handler=event_handler, player=player)
+    game_map = GameMap(map_width, map_height)
+    engine = Engine(entities=entities, event_handler=event_handler, game_map=game_map ,player=player)
 
     with tcod.context.new_terminal(screen_width,screen_height,tileset=tileset,title="Roguelike Game",vsync=True)as context:
     	root_console = tcod.Console(screen_width, screen_height, order="F")
